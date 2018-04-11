ee0a26b571e690ba38d6543b60f4c52637a09574
Dragonfury24 <dennisdnguyen19@gmail.com>
2018-04-10 20:52:42 -0400

    Add mergeSort method.

--- src/ComparingObjects.java
+++ src/ComparingObjects.java
@@ -4,12 +4,12 @@ import java.util.Random;
 public class ComparingObjects {
     public static void main(String[] args) {
         Cow[] cows = makeCows((int) (Math.random() * 10) + 1);
-        selSort(cows);
+        mergeSort(cows, 0, cows.length - 1);
         Arrays.stream(cows)
               .forEach(System.out::println);
 
         Shoe[] shoes = makeShoes((int) (Math.random() * 10) + 1);
-        selSort(shoes);
+        mergeSort(shoes, 0, shoes.length - 1);
         Arrays.stream(shoes)
               .forEach(System.out::println);
     }
@@ -57,6 +57,37 @@ public class ComparingObjects {
 
             swap(c, i, minIndex);
         }
+    }
+
+    public static void mergeSort(Comparable[] c, int low, int high) {
+        if (low < high) {
+            int middle = low + (high - low) / 2;
+            mergeSort(c,low, middle);
+            mergeSort(c, middle + 1, high);
+            merge(c, low, middle, high);
+        }
+    }
+
+    public static void merge(Comparable[] c, int low, int middle, int high) {
+        Comparable[] temp = new Comparable[c.length];
+
+        System.arraycopy(c, 0, temp, 0, c.length);
+
+        int i, j, k;
+        for (i = low, j = middle + 1, k = low; i <= middle && j <= high; k++) {
+            if (temp[i].compareTo(temp[j]) > 0) {
+                c[k] = temp[i];
+                i++;
+            } else {
+                c[k] = temp[j];
+                j++;
+            }
+        }
+
+        for ( ; i <= middle; i++, k++) {
+            c[k] = temp[i];
+        }
+
     }
 
     public static void swap(Object[] o, int index1, int index2) {
